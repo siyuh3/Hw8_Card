762b8495fafaa8a6815307d17daefea45dbd2a63
siyu3 <meikoshans@gmail.com>
2021-03-18 22:08:27 -0700

    Delete the comment inside shuffle()

--- src/Deck.java
+++ src/Deck.java
@@ -14,16 +14,16 @@ public class Deck {
     private final int NUMBER_OF_CARDS_IN_A_SUIT = 13;
     private ArrayList<Card> deck = new ArrayList<>(52);
 
-    public Deck(){
-        for (int i = 1; i <= NUMBER_OF_SUITS; i++){
-            for (int j = 1; j <= NUMBER_OF_CARDS_IN_A_SUIT; j++){
+    public Deck() {
+        for (int i = 1; i <= NUMBER_OF_SUITS; i++) {
+            for (int j = 1; j <= NUMBER_OF_CARDS_IN_A_SUIT; j++) {
                 Card newCard = new Card(i, j);
                 deck.add(newCard);
             }
         }
     }
 
-    public ArrayList<Card> getDeck(){
+    public ArrayList<Card> getDeck() {
         return deck;
     }
 
@@ -31,18 +31,8 @@ public class Deck {
      * Shuffle the cards in the deck.
      * You can implement this by randomly swapping every card in the deck.
      */
-    public void shuffleCard(){
-        /* Previous code
-        int min = 0;
-        int max = 51;
-        int range = max - min + 1;
-        for (int i = 0; i < 52; i++){
-            int rand1 = (int) (Math.random() * range) + min;
-            int rand2 = (int) (Math.random() * range) + min;
-            Collections.swap(deck, rand1, rand2);
-        }
-         */
-        for (int i = 0; i < NUMBER_OF_CARDS; i++){
+    public void shuffleCard() {
+        for (int i = 0; i < NUMBER_OF_CARDS; i++) {
             int rand1 = (int) (Math.random() * NUMBER_OF_CARDS);
             int rand2 = (int) (Math.random() * NUMBER_OF_CARDS);
             Collections.swap(deck, rand1, rand2);
@@ -52,29 +42,31 @@ public class Deck {
     /**
      * Add a new card to the deck.
      * This method should take a Card object as a parameter and add it to the ArrayList .
+     *
      * @param card Card object
      */
-    public void addCard(Card card){
+    public void addCard(Card card) {
         deck.add(card);
     }
 
     /**
      * Remove a card from the deck.
      * This removes the first card stored in the ArrayList and returns it.
+     *
      * @return Card object
      */
-    public Card removeCard(){
+    public Card removeCard() {
         return deck.remove(0);
     }
 
     /**
      * Sort the cards in the deck ordered by name.
      */
-    public void sortCard(){
+    public void sortCard() {
         int min;
-        for (int i = 0; i < deck.size() - 1; i++){
+        for (int i = 0; i < deck.size() - 1; i++) {
             min = i;
-            for (int j = i + 1; j < deck.size(); j++){
+            for (int j = i + 1; j < deck.size(); j++) {
                 if (deck.get(j).getName() < deck.get(min).getName()) min = j;
             }
             Collections.swap(deck, i, min);
@@ -83,12 +75,13 @@ public class Deck {
 
     /**
      * Print every card in the deck.
+     *
      * @return null
      */
     @Override
     public String toString() {
-        for (Card c: deck){
-            System.out.print(c.getNameAsStr()+ " "+ c.getSuitAsStr() + "\n");
+        for (Card c : deck) {
+            System.out.print(c.getNameAsStr() + " " + c.getSuitAsStr() + "\n");
         }
         return null;
     }
