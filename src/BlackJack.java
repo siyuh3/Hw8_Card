36cdeb5cbde1b12f68b1c46ea8133cb6b2355a19
siyu3 <meikoshans@gmail.com>
2021-03-20 22:22:44 -0700

    add @description

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,6 +1,12 @@
 
 import java.util.Scanner;
 
+/**
+ * @program: BlackJack
+ * @description: This class represents the BlackJake game play
+ * @author: Siyu Hou
+ * @create: 2021-03-20 23:39
+ **/
 
 // this is the class where we implement the blackjack game.
 /*
@@ -12,7 +18,6 @@ stand -- stop
 
 public class BlackJack extends Deck {
     private Player player;
-
     private int playerScore;
     private int computerScore;
     private static final int WINNING_SCORE = 21;
@@ -21,7 +26,7 @@ public class BlackJack extends Deck {
     /**
      * Default constructor.
      */
-    public BlackJack(){
+    public BlackJack() {
         this.player = Player.PLAYER;
         this.playerScore = 0;
         this.computerScore = 0;
@@ -46,15 +51,14 @@ public class BlackJack extends Deck {
     }
 
     /**
-     *
      * @return
      */
-    public Player nextPlayer(){
+    public Player nextPlayer() {
         /*
         Added nextPlayer method because there are only two players in the game, but having this
         method allows us to use the same methods for each player.
      */
-        if(this.player == Player.PLAYER){
+        if (this.player == Player.PLAYER) {
             return this.player = Player.DEALER;
         }
         /*
@@ -68,10 +72,10 @@ public class BlackJack extends Deck {
     }
 
 
-    public Card hit(){
+    public Card hit() {
         // made this return a card so the player can see what card he or she added to their score.
         Card cardValue = removeCard(); // card value must translate to an int.
-        if(this.player == Player.PLAYER){
+        if (this.player == Player.PLAYER) {
             this.playerScore += cardValue.getName();
             return cardValue;
         }
@@ -92,26 +96,23 @@ public class BlackJack extends Deck {
         BlackJack game1 = new BlackJack();
         game1.shuffleCard();
         Scanner keyboard = new Scanner(System.in);
-        while(game1.playerScore < WINNING_SCORE)
-        {
+        while (game1.playerScore < WINNING_SCORE) {
             System.out.println("Hit or Hold");
             String choice = keyboard.nextLine();
             System.out.println("\n");
-            try{
-                if(choice.equalsIgnoreCase("hit")) {
+            try {
+                if (choice.equalsIgnoreCase("hit")) {
                     Card playing = game1.hit();
                     System.out.println(playing.getName() + " of " + playing.getSuit());
                     System.out.println("player score: " + game1.playerScore);
-                }
-                else if (choice.equalsIgnoreCase("hold")){
+                } else if (choice.equalsIgnoreCase("hold")) {
                     break;
                 }
-            }
-            catch(Exception e){
+            } catch (Exception e) {
                 System.out.println("Not a valid input.");
             }
         }
-        if(game1.playerScore > 21){
+        if (game1.playerScore > 21) {
             System.out.println("\n****Bust!****\n");
         }
 
@@ -120,12 +121,12 @@ public class BlackJack extends Deck {
          */
         game1.nextPlayer();
         System.out.println("\nIt's now the " + game1.getPlayer() + " turn");
-        while(game1.computerScore < DEALER_MAX){
+        while (game1.computerScore < DEALER_MAX) {
             Card playing = game1.hit();
             System.out.println(playing.getName() + " of " + playing.getSuit());
             System.out.println("computer score: " + game1.computerScore);
         }
-        if(game1.computerScore > WINNING_SCORE){
+        if (game1.computerScore > WINNING_SCORE) {
             System.out.println("\n****Bust!****\n");
 
         /*
@@ -137,28 +138,26 @@ public class BlackJack extends Deck {
          */
         }
         // when both players have the same score and are less than 21 -- tie.
-        if(game1.playerScore == game1.computerScore &&
-                game1.computerScore < WINNING_SCORE && game1.playerScore < WINNING_SCORE)
-        {
+        if (game1.playerScore == game1.computerScore &&
+                game1.computerScore < WINNING_SCORE && game1.playerScore < WINNING_SCORE) {
             System.out.println("\npush -- no one wins.");
         }
         // when computer busts and player is less than or equal to 21. Player wins.
-        if(game1.computerScore > WINNING_SCORE && game1.playerScore <= WINNING_SCORE)
-        {
+        if (game1.computerScore > WINNING_SCORE && game1.playerScore <= WINNING_SCORE) {
             System.out.println("Player wins!!");
         }
         // both players are less than 21 and player is greater than computer -- Player wins.
 
-        else if(game1.computerScore < WINNING_SCORE && game1.playerScore <= WINNING_SCORE &&
-                game1.computerScore < game1.playerScore){
+        else if (game1.computerScore < WINNING_SCORE && game1.playerScore <= WINNING_SCORE &&
+                game1.computerScore < game1.playerScore) {
             System.out.println("\n***Player wins!!***");
         }
         // if both players go over 21.
-        else if(game1.playerScore > WINNING_SCORE && game1.computerScore > WINNING_SCORE){
+        else if (game1.playerScore > WINNING_SCORE && game1.computerScore > WINNING_SCORE) {
             System.out.println("\n No one wins.");
         }
         // otherwise computer wins.
-        else{
+        else {
             System.out.println("\n***Computer wins ***");
         }
 
