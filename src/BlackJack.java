d144127bbe2e71f51265e96151cfb97abf2613c0
Kicho 유기초 Yu <analytics20132333@gmail.com>
2021-03-21 21:45:18 -0700

    More comments!!

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -95,7 +95,7 @@ public class BlackJack extends Deck {
     Kicho
     1. We need to implement the ace case. Aces are worth 1 or 11, whichever makes a better hand.
     2. We need to show 2 cards of a player when the game starts, not 1.
-    3. We probably need to show 1 card of a dealer when the game starts. It is a design choice.
+    3. We need to show 1 card of a DEALER when the game starts. Then PLAYER starts the game. It is a design choice.
      */
     public static void main(String[] args) {
         BlackJack game1 = new BlackJack();
