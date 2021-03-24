b88a9b4f3ef498e8ed0422be4303ddfccd8c9621
Kicho 유기초 Yu <analytics20132333@gmail.com>
2021-03-24 12:44:53 -0700

    Print dealer's score on the right timing

--- src/InAndOut.java
+++ src/InAndOut.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class InAndOut {
 
-
-
     static int getInput() {
         int decision = -1;
         Scanner keyboard = new Scanner(System.in);
@@ -26,6 +24,10 @@ public class InAndOut {
         System.out.println("Player got a card: " + c.getNameAsStr() + " of " + c.getSuitAsStr());
     }
 
+    static void displayDealerCard(Card c) {
+        System.out.println("Dealer got a card: " + c.getNameAsStr() + " of " + c.getSuitAsStr());
+    }
+
     static void displayPlayerScore(BlackJack game) {
         int scoreOfPlayer = game.countScoreOfHand(game.playerHand.hand);
         System.out.println("Player's score: " + scoreOfPlayer);
@@ -34,10 +36,6 @@ public class InAndOut {
     static void displayDealerScore(BlackJack game) {
         int scoreOfPlayer = game.countScoreOfHand(game.dealerHand.hand);
         System.out.println("Dealer's score: " + scoreOfPlayer);
-    }
-
-    static void displayDealerCard(Card c) {
-        System.out.println("Dealer got a card: " + c.getNameAsStr() + " of " + c.getSuitAsStr());
     }
 
 }
