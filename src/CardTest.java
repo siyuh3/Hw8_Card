e14b52179c8e05567881f37eb5f6533d90ebae33
Kicho 유기초 Yu <analytics20132333@gmail.com>
2021-03-25 19:25:48 -0700

    Update CardTest.java with exceptions

--- src/CardTest.java
+++ src/CardTest.java
@@ -7,12 +7,18 @@ public class CardTest {
     private Card card1;
     private Card card2;
 
+    /**
+     * Setup
+     */
     @Before
     public void setUp() throws Exception {
         card1 = new Card();
         card2 = new Card(2, 2);
     }
 
+    /**
+     * Test setSuit() method
+     */
     @Test
     public void setSuit() {
         card1.setSuit(3);
@@ -21,6 +27,18 @@ public class CardTest {
         assertEquals(4, card2.getSuit());
     }
 
+    /**
+     * Tests the Exception in setSuit()
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIllegalSuit() {
+        card1.setSuit(9);
+        card2.setSuit(-2);
+    }
+
+    /**
+     * Test setName() method
+     */
     @Test
     public void setName() {
         card1.setName(3);
@@ -29,30 +47,54 @@ public class CardTest {
         assertEquals(4, card2.getName());
     }
 
+    /**
+     * Tests the Exception in setName()
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIllegalName() {
+        card1.setName(99);
+        card2.setName(-2);
+    }
+
+    /**
+     * Test getSuit() method
+     */
     @Test
     public void getSuit() {
         assertEquals(1, card1.getSuit());
         assertEquals(2, card2.getSuit());
     }
 
+    /**
+     * Test getName() method
+     */
     @Test
     public void getName() {
         assertEquals(1, card1.getName());
         assertEquals(2, card2.getName());
     }
 
+    /**
+     * Test getSuitAsStr() method
+     */
     @Test
     public void getSuitAsStr() {
         assertEquals("Spade", card1.getSuitAsStr());
         assertEquals("Heart", card2.getSuitAsStr());
     }
 
+    /**
+     * Test getNameAsStr() method
+     */
     @Test
     public void getNameAsStr() {
         assertEquals("Ace", card1.getNameAsStr());
         assertEquals("2", card2.getNameAsStr());
     }
 
+    /**
+     * Test getUnicode() method
+     */
     @Test
     public void getUnicode() {
         assertEquals("\u2660 \uD83C\uDCA1", card1.getUnicode());
